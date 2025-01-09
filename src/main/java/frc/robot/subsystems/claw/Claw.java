c96dfb89ae06f406c861155d7c6606a30b6e9de4
voidSevenSevenSix <cobbsaladluv@gmail.com>
2025-01-09 12:00:50 -0600

    base claw code

--- src/main/java/frc/robot/subsystems/claw/Claw.java
+++ src/main/java/frc/robot/subsystems/claw/Claw.java
@@ -1,9 +1,53 @@
 package frc.robot.subsystems.claw;
 
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.configs.TalonFXConfigurator;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.RobotMap;
 
 public class Claw extends SubsystemBase {
-    
+    public TalonFX rollerMotor = new TalonFX(ClawConstants.ROLLER_MOTOR_CAN_ID);
+
+    public DigitalInput beamBreak = new DigitalInput(ClawConstants.BEAM_BREAK_DIO);
+
+    public Claw(){
+        initTalons();
+    }
+
+    private void initTalons(){
+        TalonFXConfigurator cfg = rollerMotor.getConfigurator();
+        TalonFXConfiguration toApply = new TalonFXConfiguration();
+        toApply.MotorOutput.Inverted = InvertedValue.Clockwise_Positive; //TODO change this to make run intake methods work
+        toApply.MotorOutput.NeutralMode = NeutralModeValue.Coast; //TODO change this based on how tight the compression is
+        toApply.CurrentLimits.SupplyCurrentLimit = ClawCal.CLAW_ROLLERS_SUPPLY_CURRENT_LIMIT_AMPS;
+        toApply.CurrentLimits.StatorCurrentLimit = ClawCal.CLAW_ROLLERS_STATOR_SUPPLY_CURRENT_LIMIT_AMPS;
+        toApply.CurrentLimits.StatorCurrentLimitEnable = true;
+        toApply.Slot0.kP = ClawCal.CLAW_ROLLERS_P;
+        toApply.Slot0.kI = ClawCal.CLAW_ROLLERS_I;
+        toApply.Slot0.kD = ClawCal.CLAW_ROLLERS_D;
+        toApply.Slot0.kV = ClawCal.CLAW_ROLLERS_FF;
+        cfg.apply(toApply);
+    }
+
+    public boolean beamBreakSeesObject(){
+        return !beamBreak.get();
+    }
+
+    public void runMotorsIntaking(){
+        rollerMotor.set(ClawCal.CLAW_ROLLERS_INTAKING_SPEED_PERCENT);
+    }
+
+    public void runMotorsScoring(){
+        rollerMotor.set(ClawCal.CLAW_ROLLERS_SCORING_SPEED_PERCENT);
+    }
+
+    public void stopMotors(){
+        rollerMotor.set(0.0);
+    }
 }
