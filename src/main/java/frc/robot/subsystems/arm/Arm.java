18252f5af352527d22f05403ce9caa41b08de0f0
Sidharth Gujja <gujjasidharth@gmail.com>
2025-01-14 19:12:30 -0600

    Fixed a couple logical errors
    * Right motor follows left motor, use left absolute encoder
    * Removed redundant code for allowing arm movement
    * Added some boilerplate code for checking in interference zone
    * Added some sendable builder values
    * We are keeping follower to follow movement, with right being inverted from left

--- src/main/java/frc/robot/subsystems/arm/Arm.java
+++ src/main/java/frc/robot/subsystems/arm/Arm.java
@@ -3,7 +3,6 @@ package frc.robot.subsystems.arm;
 import java.util.TreeMap;
 
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.configs.TalonFXConfigurator;
 import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.hardware.CANcoder;
@@ -11,19 +10,17 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
-import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
 import frc.robot.RobotMap;
 
 public class Arm extends SubsystemBase {
     public final TalonFX armMotorLeft = new TalonFX(RobotMap.ARM_MOTOR_LEFT_CAN_ID);
     public final TalonFX armMotorRight = new TalonFX(RobotMap.ARM_MOTOR_RIGHT_CAN_ID);
 
-    private final CANcoder armRightEncoderAbs = new CANcoder(RobotMap.ARM_ABS_ENCODER_CAN_ID);
+    private final CANcoder armLefttEncoderAbs = new CANcoder(RobotMap.ARM_ABS_ENCODER_CAN_ID);
     public enum ArmPosition {
+        // home will also be the position for when we are deep climbing
         HOME,
         INTAKE,
         L2,
@@ -32,7 +29,6 @@ public class Arm extends SubsystemBase {
     }
 
     private double armDemandVolts = 0.0;
-    private boolean allowArmMovement = false;
     private double desiredSetpointPosition = 0.0;
     private double desiredSetpointVelocity = 0.0;
 
@@ -44,10 +40,10 @@ public class Arm extends SubsystemBase {
     private ArmPosition armDesiredPosition = ArmPosition.HOME;
     public Arm() {
         armPositions.put(ArmPosition.HOME, ArmCal.ARM_POSITION_HOME_DEGREES);
-        armPositions.put(ArmPosition.INTAKE, ArmCal.ARM_POSITION_L2_DEGREES);
-        armPositions.put(ArmPosition.L2, ArmCal.ARM_POSITION_L3_DEGREES);
-        armPositions.put(ArmPosition.L3, ArmCal.ARM_POSITION_L4_DEGREES);
-        armPositions.put(ArmPosition.L4, ArmCal.ARM_POSITION_INTAKE_DEGREES);
+        armPositions.put(ArmPosition.INTAKE, ArmCal.ARM_POSITION_INTAKE_DEGREES);
+        armPositions.put(ArmPosition.L2, ArmCal.ARM_POSITION_L2_DEGREES);
+        armPositions.put(ArmPosition.L3, ArmCal.ARM_POSITION_L3_DEGREES);
+        armPositions.put(ArmPosition.L4, ArmCal.ARM_POSITION_L4_DEGREES);
 
         initArmTalons();
     }
@@ -65,60 +61,60 @@ public class Arm extends SubsystemBase {
         toApply.Slot0.kD = ArmCal.ARM_MOTOR_D;
         toApply.Slot0.kV = ArmCal.ARM_MOTOR_FF;
 
-        armMotorRight.getConfigurator().apply(toApply);
-        armMotorLeft.setControl(new Follower(armMotorRight.getDeviceID(), true));
+        armMotorLeft.getConfigurator().apply(toApply);
+        armMotorRight.setControl(new Follower(armMotorLeft.getDeviceID(), true));
     }
 
     public void setDesiredPosition(ArmPosition armPosition) {
-        /**
-         * If arm is within interference zone, don't set desired position
-         */
+        // If arm is within interference zone, don't set desired position
         if (isArmMoveable()) {
             this.armDesiredPosition = armPosition;
         }
     }
 
-    public void setAllowArmMovement(boolean armMovement) {
-        this.allowArmMovement = armMovement;
-    }
     public boolean isArmMoveable() {
-        return this.allowArmMovement;
+        // return opposite of whether or not arm is in interference zone
+        return !isArmInInterferenceZone();
     }
 
     public void rezeroArm() {
-        armMotorRight.setPosition(armRightEncoderAbs.getAbsolutePosition().getValueAsDouble() * 360);
+        armMotorLeft.setPosition(armLefttEncoderAbs.getAbsolutePosition().getValueAsDouble() * 360);
     }
 
     // Account for PID when setting position of our arm
     public void controlPosition(double inputPositionDegrees) {
-        PositionVoltage desiredVoltage = new PositionVoltage(inputPositionDegrees).withSlot(0);
+        PositionVoltage desiredVoltage = new PositionVoltage(inputPositionDegrees / 360).withSlot(0);
         armMotorRight.setControl(desiredVoltage);
         // desired pos = requested pos
         this.desiredSetpointPosition = inputPositionDegrees;
         // convert velocity in rotations/sec to deg/sec
         this.desiredSetpointVelocity = armMotorRight.getVelocity().getValueAsDouble() * 360;
     }
-    // TODO: determine whether the right or left motor is the one that should be matching degree position
+
     public boolean atPositionDegrees(ArmPosition pos) {
         double checkPositionDeg = armPositions.get(pos);
-        double currentPositionDeg = armMotorRight.getPosition().getValueAsDouble() * 360;
+        double currentPositionDeg = armMotorLeft.getPosition().getValueAsDouble() * 360;
 
         return Math.abs(checkPositionDeg - currentPositionDeg) < ArmCal.ARM_MARGIN_DEGREES;
     }
-    /** TODO: Implement interference zone checking functionality */
     public boolean isArmInInterferenceZone() {
-        return Constants.PLACEHOLDER_BOOLEAN;
+        double currentPosition = armMotorLeft.getPosition().getValueAsDouble() * 360;
+        if (currentPosition <= ArmCal.ARM_INTERFERENCE_THRESHOLD_MAX_DEGREES && 
+            currentPosition >= ArmCal.ARM_INTERFERENCE_THRESHOLD_MIN_DEGREES 
+            /** TODO: AND is Deep Climb engaged */ ) {
+            return true;
+        }
+        return false;
     }
     
 
     public void stopArmMovement() {
         // left motor follows right motor, so armMotorLeft is not necessary here
-        armMotorRight.setVoltage(0.0);
+        armMotorLeft.setVoltage(0.0);
     }
 
     @Override
     public void periodic() {
-        setAllowArmMovement(isArmInInterferenceZone());
         // control arm position
         if (isArmMoveable()) {
             controlPosition(armPositions.get(this.armDesiredPosition));
@@ -131,9 +127,9 @@ public class Arm extends SubsystemBase {
         builder.addDoubleProperty("Current Position (Relative Encoder) (Deg)", (() -> armMotorRight.getPosition().getValueAsDouble() * 360), null);
         builder.addDoubleProperty("Desired Setpoint Velocity (m/s)", (() -> desiredSetpointVelocity), null);
         builder.addDoubleProperty("Arm Demand Volts (calc)", (() -> armDemandVolts), null);
-        builder.addBooleanProperty("Is Arm Movement Allowed", (() -> allowArmMovement), null);
+        builder.addBooleanProperty("Is Arm In Interference Zone", this::isArmInInterferenceZone, null);
         builder.addDoubleProperty("Motor Right Angle (Relative) ", (() -> armMotorRight.getPosition().getValueAsDouble() * 360), null);
         builder.addDoubleProperty("Motor Left Angle (Relative) ", (() -> armMotorLeft.getPosition().getValueAsDouble() * 360), null);
-        builder.addDoubleProperty("Motor Right Angle (Absolute) ", (() -> armRightEncoderAbs.getAbsolutePosition().getValueAsDouble() * 360), null);
+        builder.addDoubleProperty("Motor Right Angle (Absolute) ", (() -> armLefttEncoderAbs.getAbsolutePosition().getValueAsDouble() * 360), null);
     }
 }
