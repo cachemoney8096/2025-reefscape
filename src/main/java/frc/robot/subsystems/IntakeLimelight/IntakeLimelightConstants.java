daf2bc16eb2e9f67fd8edba224a78dd83a584a3a
avakimc <acohen1@ucls.uchicago.edu>
2025-01-22 19:47:45 -0600

    added limelight codes

--- src/main/java/frc/robot/subsystems/IntakeLimelight/IntakeLimelightConstants.java
+++ src/main/java/frc/robot/subsystems/IntakeLimelight/IntakeLimelightConstants.java
@@ -1,3 +1,11 @@
 package frc.robot.subsystems.IntakeLimelight;
 
-public class IntakeLimelightConstants {}
+import edu.wpi.first.math.util.Units;
+
+public class IntakeLimelightConstants {
+  public static final String INTAKE_LIMELIGHT_NAME = "limelight-intake";
+
+  public static final double INTAKE_LIMELIGHT_PITCH_DEGREES = 1.0;
+  public static final double INTAKE_LIMELIGHT_HEIGHT_METERS =
+      Units.inchesToMeters(12.5); // TODO: check this value physically
+}
--- src/main/java/frc/robot/subsystems/ScoringLimelight/ScoringLimelightConstants.java
+++ src/main/java/frc/robot/subsystems/ScoringLimelight/ScoringLimelightConstants.java
@@ -1,3 +1,12 @@
 package frc.robot.subsystems.ScoringLimelight;
 
-public class ScoringLimelightConstants {}
+import edu.wpi.first.math.util.Units;
+import frc.robot.Constants;
+
+public class ScoringLimelightConstants {
+  public static final String SCORING_LIMELIGHT_NAME = "limelight-intake";
+
+  public static final double SCORING_LIMELIGHT_PITCH_DEGREES = Constants.PLACEHOLDER_DOUBLE;
+  public static final double SCORING_LIMELIGHT_HEIGHT_METERS =
+      Units.inchesToMeters(Constants.PLACEHOLDER_DOUBLE); // TODO: check this value physically
+}
