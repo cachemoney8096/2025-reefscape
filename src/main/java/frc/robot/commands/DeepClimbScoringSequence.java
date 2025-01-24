f74b7a9ca6adf114ceb6f957312780ffb5aa8a92
sfkoh-afk <sfkoh2010@gmail.com>
2025-01-24 17:16:51 -0600

    deep climb scoring sequence

---
+++ src/main/java/frc/robot/commands/DeepClimbScoringSequence.java
@@ -0,0 +1,18 @@
+package frc.robot.commands;
+
+import frc.robot.subsystems.climb.Climb;
+import frc.robot.subsystems.climb.Climb.ClimbPosition;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
+
+public class DeepClimbScoringSequence extends SequentialCommandGroup  {
+    public DeepClimbScoringSequence(Climb climb) {
+        addRequirements(climb);
+        addCommands(
+            new InstantCommand(() -> climb.setClimbingPID()),
+            new InstantCommand(() -> climb.setDesiredClimbPosition(ClimbPosition.CLIMBING))
+
+        );
+    }       
+}
