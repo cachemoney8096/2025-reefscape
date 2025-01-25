e651bfbaa1a0c66b50e62be00dc4ef73a07f1db5
avakimc <acohen1@ucls.uchicago.edu>
2025-01-25 12:51:18 -0600

    removed things

--- src/main/java/frc/robot/subsystems/claw/ClawConstants.java
+++ src/main/java/frc/robot/subsystems/claw/ClawConstants.java
@@ -1,7 +1,4 @@
 package frc.robot.subsystems.claw;
 
 public class ClawConstants {
-  public static final int ROLLER_MOTOR_CAN_ID = Constants.PLACEHOLDER_INT;
-  public static final int BEAM_BREAK_DIO_1 = Constants.PLACEHOLDER_INT;
-  public static final int BEAM_BREAK_DIO_2 = Constants.PLACEHOLDER_INT;
 }
