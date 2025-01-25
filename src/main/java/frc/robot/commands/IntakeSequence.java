8ab652687495eb811dd6d02efc193a77eb17c831
Ruben Hayrapetyan <rubenhayrapetyan@rubensmac.local>
2025-01-25 17:03:23 -0600

    removed all unnecessary imports

--- src/main/java/frc/robot/commands/IntakeSequence.java
+++ src/main/java/frc/robot/commands/IntakeSequence.java
@@ -1,21 +1,15 @@
 package frc.robot.commands;
 
-import java.util.Optional;
-
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
-import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.subsystems.IntakeLimelight.IntakeLimelight;
 import frc.robot.subsystems.arm.Arm;
 import frc.robot.subsystems.arm.Arm.ArmPosition;
 import frc.robot.subsystems.claw.Claw;
 import frc.robot.subsystems.elevator.Elevator;
 import frc.robot.subsystems.elevator.Elevator.ElevatorHeight;
-import edu.wpi.first.math.geometry.Transform2d;
-import java.util.Optional;
 
 public class IntakeSequence extends SequentialCommandGroup {
     public IntakeSequence(Claw claw, IntakeLimelight light, Arm arm, Elevator elevator) {
