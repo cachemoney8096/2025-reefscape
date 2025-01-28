be277841029944e030f20020be9c1406532c80a8
Blacktail6655 <joalenghat@gmail.com>
2025-01-28 17:07:13 -0600

    Added limelight to DeepClimbPrep

--- src/main/java/frc/robot/commands/DeepClimbPrep.java
+++ src/main/java/frc/robot/commands/DeepClimbPrep.java
@@ -1,16 +1,41 @@
 package frc.robot.commands;
+import java.util.Optional;
+
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.subsystems.climb.Climb;
+import frc.robot.subsystems.IntakeLimelight.IntakeLimelight;
+import frc.robot.subsystems.arm.Arm;
+import frc.robot.subsystems.arm.Arm.ArmPosition;
 import frc.robot.subsystems.climb.Climb.ClimbPosition;
+import frc.robot.subsystems.elevator.Elevator;
+import frc.robot.subsystems.elevator.Elevator.ElevatorHeight;
+import edu.wpi.first.math.geometry.Transform2d;
 
 public class DeepClimbPrep extends SequentialCommandGroup {
-    public DeepClimbPrep(Climb climb) {
+    private Optional<Transform2d> isTag = Optional.empty();
+    public DeepClimbPrep(Climb climb, IntakeLimelight intakeLime, Elevator elevator, Arm arm) {
       addRequirements(climb);
-      // TODO use apriltag to allign with cage
+      isTag = intakeLime.checkForTag();
+
       addCommands(
-        new WaitUntilCommand(() -> climb.isClimbMovable()),
-        new InstantCommand(() -> climb.setDesiredClimbPosition(ClimbPosition.CLIMBING_PREP)));
+        new ConditionalCommand(
+              new SequentialCommandGroup(
+                //TODO drive logic here
+                //drive should happen at the same time as the next 5 commands
+                new InstantCommand(() -> elevator.setDesiredPosition(ElevatorHeight.HOME)),
+                new WaitUntilCommand(() -> arm.isArmMoveable()),
+                new InstantCommand(() -> arm.setDesiredPosition(ArmPosition.DEEP_CLIMB)),
+                new WaitUntilCommand(() -> climb.isClimbMovable()),
+                new InstantCommand(() -> climb.setDesiredClimbPosition(ClimbPosition.CLIMBING_PREP))
+              ),
+          new InstantCommand(), // return out of the command if the robot does not see the intake april tag 
+          () -> {return !isTag.isEmpty();}
+        )
+      );
+
     }
   }
