f030bb92be59afca4e708a8d55c2cf806946aa33
Ruben Hayrapetyan <rubenhayrapetyan@rubensmac.local>
2025-01-28 19:31:57 -0600

    fixed ava changes

--- src/main/java/frc/robot/commands/IntakeSequence.java
+++ src/main/java/frc/robot/commands/IntakeSequence.java
@@ -1,6 +1,8 @@
 package frc.robot.commands;
 
 import java.util.Optional;
+import java.util.concurrent.locks.Condition;
+
 import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.ConditionalCommand;
@@ -11,26 +13,39 @@ import frc.robot.subsystems.IntakeLimelight.IntakeLimelight;
 import frc.robot.subsystems.arm.Arm;
 import frc.robot.subsystems.arm.Arm.ArmPosition;
 import frc.robot.subsystems.claw.Claw;
+import frc.robot.subsystems.climb.Climb;
+import frc.robot.subsystems.climb.Climb.ClimbPosition;
 import frc.robot.subsystems.elevator.Elevator;
 import frc.robot.subsystems.elevator.Elevator.ElevatorHeight;
 
 public class IntakeSequence extends SequentialCommandGroup {
     private Optional<Transform2d> isTag = Optional.empty();
-    public IntakeSequence(Claw claw, IntakeLimelight intakeLime, Arm arm, Elevator elevator) {
+    public IntakeSequence(Claw claw, IntakeLimelight intakeLime, Arm arm, Elevator elevator, Climb climb) {
         isTag = intakeLime.checkForTag();
+        SequentialCommandGroup moveArmElevatorClaw = new SequentialCommandGroup(
+            /*TODO do drive logic here*/
+            // the next two need to be done at the same time as the driving
+            new ConditionalCommand(
+                    new SequentialCommandGroup(
+                        new InstantCommand(() -> climb.setDesiredClimbPosition(ClimbPosition.CLEAR_OF_ARM)),
+                        new InstantCommand(() -> arm.setDesiredPosition(ArmPosition.INTAKE))
+                    ),
+                    new InstantCommand(() -> arm.setDesiredPosition(ArmPosition.INTAKE)), 
+                    () -> {return arm.isArmMoveable();}),
+            new InstantCommand(() -> elevator.setDesiredPosition(ElevatorHeight.HOME)),
+            new InstantCommand(() -> claw.runMotorsIntaking()),
+            new WaitUntilCommand(claw::beamBreakSeesObject),
+            new InstantCommand(() -> claw.stopMotors())
+        );
         addRequirements(claw);
         addCommands(
             new ConditionalCommand(
                                     new SequentialCommandGroup(
                                         /*TODO do drive logic here*/
                                         // the next two need to be done at the same time as the driving
-                                        new InstantCommand(() -> arm.setDesiredPosition(ArmPosition.INTAKE)),
-                                        new InstantCommand(() -> elevator.setDesiredPosition(ElevatorHeight.HOME)),
-                                        new InstantCommand(() -> claw.runMotorsIntaking()),
-                                        new WaitUntilCommand(claw::beamBreakSeesObject),
-                                        new InstantCommand(() -> claw.stopMotors())
+                                        moveArmElevatorClaw
                                     ), 
-                                    new InstantCommand(), // return out of the command if the robot does not see the intake april tag 
+                                    moveArmElevatorClaw, // return out of the command if the robot does not see the intake april tag 
                                     () -> {return (!isTag.isEmpty() && (
                                         (NetworkTableInstance.getDefault().getTable("limelight").getEntry("tid").getDouble(0) == 1) ||
                                         (NetworkTableInstance.getDefault().getTable("limelight").getEntry("tid").getDouble(0) == 2) ||
