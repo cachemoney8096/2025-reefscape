8207a1d66df22d62523dcf9182db78479ba059b9
Huck Bishop <Waxy1221@gmail.com>
2025-01-29 18:32:22 -0600

    Changed Base X and Y Values to Meters

--- src/main/java/frc/robot/utils/HPStationTransfromUtil.java
+++ src/main/java/frc/robot/utils/HPStationTransfromUtil.java
@@ -1,11 +1,12 @@
 package frc.robot.utils;
 
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.util.Units;
 
 public class HPStationTransfromUtil {
 
-    private final double BASE_X_TRANSFORM = 10.6;
-    private final double BASE_Y_TRANSFORM = 14.6;
+    private final double BASE_X_TRANSFORM_METERS = Units.inchesToMeters(10.6);
+    private final double BASE_Y_TRANSFORM_METERS = Units.inchesToMeters(14.6);
 
     // The left or right HP station
     public enum Station {
@@ -20,8 +21,8 @@ public class HPStationTransfromUtil {
     }
 
     public Translation2d getHPStationTransfrom(Station station, Position position, boolean isBlueTeam) {
-        double x = BASE_X_TRANSFORM;
-        double y = BASE_Y_TRANSFORM;
+        double x = BASE_X_TRANSFORM_METERS;
+        double y = BASE_Y_TRANSFORM_METERS;
 
         if (station == Station.RIGHT) {
             y *= -1;
