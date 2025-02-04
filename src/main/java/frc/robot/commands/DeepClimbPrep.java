c72d429224de5293f038e3c68990e2c992a217df
Blacktail6655 <joalenghat@gmail.com>
2025-02-04 17:42:19 -0600

    Conditional commands and arm logic
    zain helped i guess

--- src/main/java/frc/robot/commands/DeepClimbPrep.java
+++ src/main/java/frc/robot/commands/DeepClimbPrep.java
@@ -1,7 +1,7 @@
 package frc.robot.commands;
 import java.util.Optional;
+import java.util.Arrays;
 
-import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
@@ -14,35 +14,38 @@ import frc.robot.subsystems.arm.Arm.ArmPosition;
 import frc.robot.subsystems.climb.Climb.ClimbPosition;
 import frc.robot.subsystems.elevator.Elevator;
 import frc.robot.subsystems.elevator.Elevator.ElevatorHeight;
-import edu.wpi.first.math.geometry.Transform2d;
 
 public class DeepClimbPrep extends SequentialCommandGroup {
-    private Optional<Transform2d> isTag = Optional.empty();
-    private double tagName = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tid").getDouble(0);
     public DeepClimbPrep(Climb climb, IntakeLimelight intakeLimelight, Elevator elevator, Arm arm) {
       addRequirements(climb);
-      isTag = intakeLimelight.checkForTag();
-      
+      Double[] climbScoringIds = new Double[] {4.0, 5.0, 14.0, 15.0};
         addCommands(
         new ConditionalCommand(
               new SequentialCommandGroup(
                 //TODO drive logic here
                 //drive should happen at the same time as the next 5 commands
                 new InstantCommand(() -> elevator.setDesiredPosition(ElevatorHeight.HOME)),
-                new ConditionalCommand(null, null, null)
-                new InstantCommand(() -> arm.setDesiredPosition(ArmPosition.DEEP_CLIMB)),
-                new WaitUntilCommand(() -> climb.isClimbMovable()),
-                new InstantCommand(() -> climb.setDesiredClimbPosition(ClimbPosition.CLIMBING_PREP))
+                new ConditionalCommand(
+                  new InstantCommand(() -> arm.setDesiredPosition(ArmPosition.DEEP_CLIMB)), 
+                  new InstantCommand(),  
+                  () -> {return arm.isArmInInterferenceZone();}),
+                new ConditionalCommand(
+                  new InstantCommand(() -> elevator.setDesiredPosition(ElevatorHeight.HOME)), 
+                  new InstantCommand(),  
+                  () -> {return climb.isClimbInInterferenceZone();})
               ),
               new SequentialCommandGroup(
-                
                 new InstantCommand(() -> elevator.setDesiredPosition(ElevatorHeight.HOME)),
-                new WaitUntilCommand(() -> arm.isArmMoveable()),
-                new InstantCommand(() -> arm.setDesiredPosition(ArmPosition.DEEP_CLIMB)),
-                new WaitUntilCommand(() -> climb.isClimbMovable()),
-                new InstantCommand(() -> climb.setDesiredClimbPosition(ClimbPosition.CLIMBING_PREP))
+                new ConditionalCommand(
+                  new InstantCommand(() -> arm.setDesiredPosition(ArmPosition.DEEP_CLIMB)), 
+                  new InstantCommand(),  
+                  () -> {return arm.isArmInInterferenceZone();}),
+                new ConditionalCommand(
+                  new InstantCommand(() -> elevator.setDesiredPosition(ElevatorHeight.HOME)), 
+                  new InstantCommand(),  
+                  () -> {return climb.isClimbInInterferenceZone();})
               ), // prep for manual if april tag not detected 
-          () -> {return IntakeLimelight.validDeepClimbTag(tagName);}
+          () -> {return Arrays.asList(climbScoringIds).contains(NetworkTableInstance.getDefault().getTable("limelight").getEntry("tid").getDouble(0));}
         )
       );
       
