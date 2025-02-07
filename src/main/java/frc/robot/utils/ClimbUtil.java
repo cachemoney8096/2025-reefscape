043d03c11757dfe993189461f85d3f28f5358106
voidSevenSevenSix <cobbsaladluv@gmail.com>
2025-02-07 09:00:29 -0600

    refactor climb util for readability and ease of use

--- src/main/java/frc/robot/utils/ClimbUtil.java
+++ src/main/java/frc/robot/utils/ClimbUtil.java
@@ -5,7 +5,7 @@ import edu.wpi.first.math.util.Units;
 import frc.robot.Constants;
 
 public class ClimbUtil {
-  private final double LOCATION_OFFSET_Y_METERS =
+  private static final double LOCATION_OFFSET_Y_METERS =
       Units.inchesToMeters(Constants.CLIMB_CAGE_LOCATION_OFFSET_INCHES);
 
   public enum CagePosition {
@@ -14,31 +14,14 @@ public class ClimbUtil {
     CENTER
   }
 
-  private CagePosition cagePosition = CagePosition.CENTER;
-
-  public CagePosition getCagePosition() {
-    return this.cagePosition;
-  }
-
-  public void setCagePosition(CagePosition cagePosition) {
-    this.cagePosition = cagePosition;
-  }
-
-  public Translation2d getClimbTransform(CagePosition cagePosition, boolean isBlueTeam) {
+  public Translation2d getClimbTransform(CagePosition cagePosition, boolean isRed) {
     double y = LOCATION_OFFSET_Y_METERS;
-
-    if (cagePosition == CagePosition.LEFT) {
-      y *= -1;
-    } else if (cagePosition == CagePosition.CENTER) {
-      y = 0.0;
+    if(cagePosition == CagePosition.RIGHT){
+        y *= -1;
     }
-
-    if (!isBlueTeam) {
-      y *= -1;
+    if(isRed){
+        y *= -1;
     }
-
-    Translation2d out = new Translation2d(0.0, y);
-
-    return (out);
+    return new Translation2d(0.0, y);
   }
 }
