8742a8bbed1889b7abd3e2d186520a2aae69f1e2
Zain Zaidi <zaidichicago@gmail.com>
2025-02-25 17:07:56 -0600

    Changes to deep climb scoring squence

--- src/main/java/frc/robot/commands/DeepClimbScoringSequence.java
+++ src/main/java/frc/robot/commands/DeepClimbScoringSequence.java
@@ -4,15 +4,22 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.subsystems.arm.Arm;
+import frc.robot.subsystems.arm.Arm.ArmPosition;
 import frc.robot.subsystems.climb.Climb;
 import frc.robot.subsystems.climb.Climb.ClimbPosition;
+import frc.robot.subsystems.elevator.Elevator;
+import frc.robot.subsystems.elevator.Elevator.ElevatorHeight;
 
 public class DeepClimbScoringSequence extends SequentialCommandGroup {
-  public DeepClimbScoringSequence(Arm arm, Climb climb) {
+  public DeepClimbScoringSequence(Arm arm, Climb climb, Elevator elevator) {
     addRequirements(climb);
     addCommands(
+        new WaitUntilCommand(() -> elevator.atDesiredPosition(ElevatorHeight.HOME)),
+        new WaitUntilCommand(() -> (arm.atDesiredArmPosition(ArmPosition.DEEP_CLIMB)),
         new WaitUntilCommand(() -> climb.atClimbPosition(ClimbPosition.CLIMBING_PREP)),
         new InstantCommand(() -> climb.setClimbingPID()),
-        new InstantCommand(() -> climb.setDesiredClimbPosition(ClimbPosition.CLIMBING)));
-  }
+        new InstantCommand(() -> climb.setDesiredClimbPosition(ClimbPosition.CLIMBING))
+    );
+}
+
 }
