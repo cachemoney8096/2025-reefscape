070f23d6a8b6374817d81348c54d4d16feeffb6c
Sidharth Gujja <gujjasidharth@gmail.com>
2025-02-26 16:54:57 -0600

    im stupid

--- src/main/java/frc/robot/commands/DeepClimbScoringSequence.java
+++ src/main/java/frc/robot/commands/DeepClimbScoringSequence.java
@@ -3,14 +3,13 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
-import frc.robot.subsystems.arm.Arm;
 import frc.robot.subsystems.climb.Climb;
 import frc.robot.subsystems.climb.Climb.ClimbPosition;
 import frc.robot.subsystems.elevator.Elevator;
 import frc.robot.subsystems.elevator.Elevator.ElevatorHeight;
 
 public class DeepClimbScoringSequence extends SequentialCommandGroup {
-  public DeepClimbScoringSequence(Arm arm, Climb climb, Elevator elevator) {
+  public DeepClimbScoringSequence(Climb climb, Elevator elevator) {
     addRequirements(climb, elevator);
     addCommands(
         new WaitUntilCommand(() -> elevator.atElevatorPosition(ElevatorHeight.HOME)),
