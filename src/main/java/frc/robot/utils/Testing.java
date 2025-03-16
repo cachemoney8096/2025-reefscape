a684bfa779fda0c536561022d5882306c0081509
voidSevenSevenSix <cobbsaladluv@gmail.com>
2025-03-15 21:10:13 -0500

    fix code crashing :skull:

---
+++ src/main/java/frc/robot/utils/Testing.java
@@ -0,0 +1,27 @@
+package frc.robot.utils;
+
+import java.util.HashMap;
+
+import edu.wpi.first.math.Pair;
+
+public class Testing {
+    public enum Station {
+    LEFT,
+    RIGHT
+  }
+
+  public enum Position {
+    LEFT,
+    RIGHT,
+    CENTER
+  }
+
+  private static HashMap<Pair<Station, Position>, Pair<Double, Double>> blueMap =
+      new HashMap<Pair<Station, Position>, Pair<Double, Double>>();
+
+  public static void main(String[] args){
+    blueMap.put(
+        new Pair<Station, Position>(Station.LEFT, Position.LEFT),
+        new Pair<Double, Double>(-1.0, -1.0));
+  }
+}
