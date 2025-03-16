24bb23917c8b97a44627f2c7b11ee968f96e7e96
voidSevenSevenSix <cobbsaladluv@gmail.com>
2025-03-16 16:54:22 -0500

    arm works again

--- src/main/java/frc/robot/subsystems/arm/ArmCal.java
+++ src/main/java/frc/robot/subsystems/arm/ArmCal.java
@@ -1,11 +1,11 @@
 package frc.robot.subsystems.arm;
 
     public class ArmCal {
-  public static final double ARM_POSITION_INTAKE_DEGREES = 88.6, // 141.5 - 57,
-      ARM_POSITION_HOME_DEGREES = 141.5, // 108
-      ARM_POSITION_L1_DEGREES = 141.5 + 42,
-      ARM_POSITION_L2_DEGREES = 141.5 + 42,
-      ARM_POSITION_L3_DEGREES = 141.5 + 42 + 13,
+  public static final double ARM_POSITION_INTAKE_DEGREES = 168.3 - 52.9, // 141.5 - 57,
+      ARM_POSITION_HOME_DEGREES = 168.3, // 108
+      ARM_POSITION_L1_DEGREES = 168.3 + 42,
+      ARM_POSITION_L2_DEGREES = 168.3 + 42,
+      ARM_POSITION_L3_DEGREES = 168.3 + 42 + 13,
       ARM_POSITION_L4_DEGREES = 200.0, // TODO this is wrong
       ARM_POSITION_DEEP_CLIMB_DEGREES = 214; // 141.5 + 29;
   public static final double ARM_SUPPLY_CURRENT_LIMIT_AMPS = 80.0,
@@ -16,7 +16,7 @@ package frc.robot.subsystems.arm;
 
   public static final double ARM_MOTOR_P = 0.75, // should be 2.5 when not having coral
       ARM_MOTOR_I = 0.0,
-      ARM_MOTOR_D = -0.1, // should be 0.0 when not having coral
+      ARM_MOTOR_D = 0.0, // should be 0.0 when not having coral
       ARM_MOTOR_FF = 0.0;
 
   public static final double MOTOR_TO_ARM_ROTATIONS = 90.0; //90 rotations of motor to 1 arm rotation
