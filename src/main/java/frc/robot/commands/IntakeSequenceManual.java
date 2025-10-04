801c88380011e09d92727719e0e822e5bb9c8e2a
SaanikaGW <dutta.saanika@gmail.com>
2025-10-04 08:35:40 -0500

    Updated  button-bind commands

---
+++ src/main/java/frc/robot/commands/IntakeSequenceManual.java
@@ -0,0 +1,66 @@
+package frc.robot.commands;
+
+import java.util.function.Consumer;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
+import frc.robot.subsystems.arm.Arm;
+import frc.robot.subsystems.arm.Arm.ArmPosition;
+import frc.robot.subsystems.claw.Claw;
+import frc.robot.subsystems.elevator.Elevator;
+import frc.robot.subsystems.elevator.Elevator.ElevatorHeight;
+
+public class IntakeSequenceManual extends SequentialCommandGroup {
+    public enum Location {
+        LEFT,
+        RIGHT
+      }
+
+    public IntakeSequenceManual(
+      Arm arm,
+      Elevator elevator,
+      Claw claw,
+      Location location,
+      Consumer<Double> headingSetter
+  ) {
+     
+    //PLACEHOLDERS
+    final double headingLeft  = -15.0;
+    final double headingRight = 15.0;
+
+    addCommands(
+        new InstantCommand(() -> {
+         
+          if (location == Location.LEFT) {
+            headingSetter.accept(headingLeft);
+          } else {
+            headingSetter.accept(headingRight);
+          }
+
+         
+          arm.setDesiredPosition(ArmPosition.INTAKE);
+          elevator.setDesiredPosition(ElevatorHeight.INTAKE);
+          claw.runMotorsIntaking();
+        }, arm, elevator, claw), 
+
+        new WaitUntilCommand(claw::beamBreakSeesObject),
+
+        new InstantCommand(claw::stopMotors, claw),
+
+        new WaitCommand(2.0),
+
+        new InstantCommand(() -> {
+          arm.setDesiredPosition(ArmPosition.HOME);
+          elevator.setDesiredPosition(ElevatorHeight.HOME);
+        }, arm, elevator)
+    );
+
+    addRequirements(arm, elevator, claw);
+
+    this.handleInterrupt(claw::stopMotors);
+    
+}
+
+}
---
+++ src/main/java/frc/robot/commands/PrepScoreManual.java
@@ -0,0 +1,13 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
+import frc.robot.subsystems.arm.Arm;
+import frc.robot.subsystems.elevator.Elevator;
+import frc.robot.subsystems.arm.Arm.ArmPosition;
+import frc.robot.subsystems.elevator.Elevator.ElevatorHeight;
+
+public class PrepScoreManual extends SequentialCommandGroup{
+    
+}
