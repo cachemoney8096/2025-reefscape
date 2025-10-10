183dd0f0ac710fb65657cea6a3addaee5b79365c
voidSevenSevenSix <cobbsaladluv@gmail.com>
2025-10-09 22:22:19 -0500

    dumbdrive upgrades

--- src/main/java/frc/robot/commands/DumbDrive.java
+++ src/main/java/frc/robot/commands/DumbDrive.java
@@ -8,6 +8,7 @@ import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.LimelightHelpers;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 
@@ -18,6 +19,7 @@ public class DumbDrive extends SequentialCommandGroup{
             System.out.println("no tag seen");
             return;
         }
+        
         System.out.println("dumbdrive activated");
         double targetHeadingDeg = heading - Math.toDegrees(tagPose3dRobotSpace.getRotation().getY());
         double speedMetersPerSecond = 0.2;
@@ -27,14 +29,24 @@ public class DumbDrive extends SequentialCommandGroup{
         double yDistMeters = tagPose3dRobotSpace.getX() + horizontalOffsetMeters; //side to side
         double timeToTagSeconds = xDistMeters / speedMetersPerSecond;
         double timeToAlignSeconds = yDistMeters / speedMetersPerSecond;
+        
         addCommands(
             new InstantCommand(()->{headingSetter.accept(targetHeadingDeg);}),
+            new InstantCommand(()->System.out.println("set target heading, waiting 0.5s")),
             new WaitCommand(0.5),
             new InstantCommand(()->{rrVelocitySetter.accept(0.0, speedMetersPerSecond);}),
+            new InstantCommand(()->System.out.println("set horizontal motion, waiting to align")),
             new WaitCommand(timeToAlignSeconds),
+            //new WaitUntilCommand(() -> Math.abs(LimelightHelpers.getTX(llName)) < 2.0),
             new InstantCommand(()->{rrVelocitySetter.accept(speedMetersPerSecond, 0.0);}),
+            new InstantCommand(()->System.out.println("set vertical motion, waiting to align")),
             new WaitCommand(timeToTagSeconds),
-            new InstantCommand(()->{rrVelocitySetter.accept(0.0, 0.0);})
+            //new WaitUntilCommand(() -> Math.abs(LimelightHelpers.getTZ(llName) - targetDistanceMeters) < 0.05)
+            new InstantCommand(()->{rrVelocitySetter.accept(0.0, 0.0);}),
+            new InstantCommand(()->System.out.println("finished, set to zero"))
         );
+
+        this.until(()->joystickInput.get());
+        this.finallyDo(()->rrVelocitySetter.accept(0.0, 0.0));
     }
 }
